60196d38ebfc49b9d2f7e7cf4696b9b8396bb48d
Gaëtan Renaudeau <gre@zenexity.com>
2013-09-26 10:52:20 +0200

    latest

--- audioutils.js
+++ audioutils.js
@@ -60,11 +60,34 @@ var createDivHere = (function (i) {
   };
 }(1));
 
-var hookRevealJS = function (f) {
+var hookRevealJS = function (f, noSourceCode) {
   var div = createDivHere();
+  if (!noSourceCode) addToggleSourceCode();
   var parents = $(div).parents("section");
   var currentSectionH = parents.last().prevAll("section").size();
   var currentSectionV = parents.size()<=1 ? 0 : $(div).parents("section").first().prevAll("section").size();
+  
+  function addToggleSourceCode () {
+    var d = $(div);
+    var toggle = $('<a href="#" class="toggle-source">Toggle Source Code</a>');
+    var sc = $('<pre style="clear:both"></pre>');
+    var code = $('<code data-trim />').html(f.toString()).appendTo(sc);
+    toggle.on("click", function (e) {
+      e.preventDefault();
+      if (sc.is(":visible")) {
+        d.show();
+        sc.hide();
+      }
+      else {
+        d.hide();
+        sc.show();
+      }
+    });
+    sc.hide();
+    d.after(sc);
+    d.after(toggle);
+  }
+  
   $(function(){
     var end;
     var lastH, lastV;
@@ -190,7 +213,7 @@ KeyboardController = Backbone.Model.extend({
   defaults: _.extend({
     octave: 4,
     keyCodeByTones: []
-  }, AZERTYconfig),
+  }, QWERTYconfig),
   initialize: function () {
     $(window).on("keydown", _.bind(this.onKeydown, this));
     $(window).on("keyup", _.bind(this.onKeyup, this));
@@ -229,7 +252,7 @@ KeyboardController = Backbone.Model.extend({
         e.preventDefault();
         var note = this.get("octave")*12+tone;
         if (note >= 0 && note <= 127) {
-          this.trigger("noteOn", note, 127);
+          this.trigger("noteOn", note, 1);
         }
       }
     }
@@ -237,6 +260,104 @@ KeyboardController = Backbone.Model.extend({
 }, {
   AZERTYconfig: AZERTYconfig,
   QWERTYconfig: QWERTYconfig
+});
+
+}());
+
+(function(){
+
+MIDIController = Backbone.Model.extend({
+  initialize: function () {
+    if ( !("requestMIDIAccess" in navigator) ) {
+      console.log("requestMIDIAccess is not supported by your browser.");
+      this.set("state", "error", new Error("MIDI not supported by your browser"));
+      return;
+    }
+
+    this.set("state", "loading");
+    this.midiAccessPromise = navigator.requestMIDIAccess();
+    this.midiAccessPromise.then(_.bind(this.onMidiSuccess, this), _.bind(this.onMidiError, this));
+  },
+
+  bindInput: function (input) {
+    input.onmidimessage = _.bind(this.onMidiMessage, this);
+  },
+
+  noteOn: function (noteNumber, noteVelocity) {
+    this.trigger("noteOn", noteNumber, noteVelocity);
+  },
+
+  noteOff: function (noteNumber, noteVelocity) {
+    this.trigger("noteOff", noteNumber, noteVelocity);
+  },
+
+  control: function (controlId, value) {
+    this.trigger("control", controlId, value);
+  },
+
+  onMidiMessage: function (message) {
+    // @see http://www.midi.org/techspecs/midimessages.php 
+    var status = message.data[0];
+    var byte2 = message.data[1];
+    var byte3 = message.data[2];
+    // We only handle Channel 1 here...
+    switch (status) {
+      case 128: // NOTE OFF
+        this.noteOff(byte2, byte3/127);
+        break;
+      case 144: // NOTE ON
+        this.noteOn(byte2, byte3/127);
+        break;
+      case 176: // control change
+        this.control(byte2, byte3);
+        break;
+      case 192: // program change
+        this.control("prog", byte2); // consider it as control
+        break;
+      case 224:
+        this.control("PB", byte3+byte2/127);
+        break;
+      default:
+        console.log( "Unknown status code : " + status);
+    }
+  },
+
+  onMidiSuccess: function (midiAccess) {
+    // OUCH this is not working! It seems we have to reboot the browser each time!
+    midiAccess.onconnect = function () {
+      console.log("onconnect", arguments);
+    }
+    midiAccess.ondisconnect = function () {
+      console.log("ondisconnect", arguments);
+    }
+
+    var inputs = midiAccess.inputs();
+    console.log("MIDI inputs: ", inputs);
+
+    /*
+    for (i=0; i<inputs.length; i++) {
+      console.log( "Input port #" + i + 
+          ": type:'" + inputs[i].type +
+          "' id:'" + inputs[i].id +
+          "' manufacturer:'" + inputs[i].manufacturer +
+          "' name:'" + inputs[i].name +
+          "' version:'" + inputs[i].version + "'" );
+    }
+    */
+
+    if (inputs.length) {
+      _.each(inputs, this.bindInput, this);
+      this.set("state", "success");
+    }
+    else {
+      this.set("state", "noinputs");
+    }
+  },
+  onMidiError: function (error) {
+    console.log("MIDI Access failed:", error);
+    this.set("state", "error", error);
+  }
+
 });
 
 }());
